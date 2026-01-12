9d8c56f6da7da3745b481f7b6db45a3377caaf3c
vpattar <vishwap37@yahoo.com>
2026-01-11 23:43:33 -0800

    Remove custom deploy file

---
+++ config.js
@@ -0,0 +1,3 @@
+// Local development configuration - ideally should not be committed to version control
+window.GOOGLE_APPS_SCRIPT_URL='https://script.google.com/macros/s/AKfycbxF6RqEA13RHeVlFFcyv8uq2Gt6GSo8dfXGPYOq-IsKXZkO087zjAcfYzhuGKRBOzGSZQ/exec'
+window.GA_TRACKING_ID='G-0REE9FK2W4'
